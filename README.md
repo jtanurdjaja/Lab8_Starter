d3d09fccc6d8d8152884233b248dbfbda766ad7d
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2023-05-28 01:26:33 -0700

    Updated README, completed part 2

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Intro
 
-1. Within a Github action that runs whenever code is pushed
-2. Yes
-3. 
+1. I would fit my automated tests within a Github action that runs whenever code is pushed. The Github actions can be used to prevent people from pushing broken code because it can be set so that people cannot merge code unless they pass the tests run by Github actions.
+2. No, because E2E testing is used to automate test cases that involve emulating user actions. I would use unit testing to check if a function returns the correct output because we are simply checking if the function will return a particular output. We are not emulating any user action in this case.
+3. No, I do not think that this is the right use of unit testing because we are testing how individual components are interacting with each other. In this case, we are testing the message feature, which encapsulates components on sender and receiver ends.
+4. Yes, I think that this is a feasible use of unit testing because we are only testing an individual part of the code. We can simply test inputs that are greater than 80 characters to see if the function will return false. We can also test if the general expected inputs are possible.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,72 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// testing isPhoneNumber function
+test('phone number with ten numbers', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('phone numbers with parentheses', () => {
+    expect(functions.isPhoneNumber('(555)-456-7890')).toBe(true);
+});
+test('phone number with too little numbers', () => {
+    expect(functions.isPhoneNumber('7890')).toBe(false);
+});
+test('phone number with too many numbers', () => {
+    expect(functions.isPhoneNumber('5554567891891081')).toBe(false);
+});
+
+// testing isEmail function
+test('email with @ and .<string>', () => {
+    expect(functions.isEmail('jtanurdjaja@ucsd.edu')).toBe(true);
+});
+test('email with underscores', () => {
+    expect(functions.isEmail('j_tan@ucsd.edu')).toBe(true);
+});
+test('no email input', () => {
+    expect(functions.isEmail('')).toBe(false);
+});
+test('email missing @', () => {
+    expect(functions.isEmail('jtanurdjaja.com')).toBe(false);
+});
+
+// testing isStrongPassword function
+test('password that is longer than 3 characters', () => {
+    expect(functions.isStrongPassword('abcde')).toBe(true);
+});
+test('password that contains numbers and underscores', () => {
+    expect(functions.isStrongPassword('j_tan123')).toBe(true);
+});
+test('password starting with underscore', () => {
+    expect(functions.isStrongPassword('_hhh')).toBe(false);
+});
+test('password only containing numbers', () => {
+    expect(functions.isStrongPassword('1234')).toBe(false);
+});
+
+// testing isDate function
+test('Date using forward slashes', () => {
+    expect(functions.isDate('03/02/2021')).toBe(true);
+});
+test('Date with month and day as single numbers', () => {
+    expect(functions.isDate('2/5/2023')).toBe(true);
+});
+test('Date using dashes', () => {
+    expect(functions.isDate('03-02-2021')).toBe(false);
+});
+test('Date with year as two digits', () => {
+    expect(functions.isDate('03/02/21')).toBe(false);
+});
+
+// testing isHexColor function
+test('Hex color starts # and numbers', () => {
+    expect(functions.isHexColor('#A12')).toBe(true);
+});
+test('Hex color with less than 5 characters', () => {
+    expect(functions.isHexColor('#ABC')).toBe(true);
+});
+test('Hex color with letters out of range', () => {
+    expect(functions.isHexColor('KKKKKK')).toBe(false);
+});
+test('Hex color with more than 6 characters', () => {
+    expect(functions.isHexColor('ABBBBBB')).toBe(false);
+});
